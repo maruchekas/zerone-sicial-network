52e130a0534f51cd03ebe758ec710a0183b1b6f8
Ivan Zu2kel <ivan_zukkel@mail.ru>
2021-12-14 20:56:03 +0300

    Добавлен Jwt токен.

--- src/main/java/com/skillbox/javapro21/config/SecurityConfig.java
+++ src/main/java/com/skillbox/javapro21/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.skillbox.javapro21.config;
 
+import com.skillbox.javapro21.api.security.JwtFilter;
 import com.skillbox.javapro21.api.security.UserDetailServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -22,10 +23,13 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 */
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    private final JwtFilter jwtFilter;
+
     private final UserDetailServiceImpl userDetailServiceImpl;
 
     @Autowired
-    public SecurityConfig(UserDetailServiceImpl userDetailServiceImpl) {
+    public SecurityConfig(JwtFilter jwtFilter, UserDetailServiceImpl userDetailServiceImpl) {
+        this.jwtFilter = jwtFilter;
         this.userDetailServiceImpl = userDetailServiceImpl;
     }
 
@@ -42,7 +46,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().disable()
                 .httpBasic().disable()
-                .addFilterBefore(null, UsernamePasswordAuthenticationFilter.class);
+                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
     }
 
     @Bean
