08e469e3b626418e3a7f4473ea451d2cd853cd1b
Ivan Zu2kel <ivan_zukkel@mail.ru>
2021-12-15 22:51:49 +0300

    Образец логирования

--- src/main/java/com/skillbox/javapro21/controller/AccountController.java
+++ src/main/java/com/skillbox/javapro21/controller/AccountController.java
@@ -6,6 +6,7 @@ import com.skillbox.javapro21.exception.UserExistException;
 import com.skillbox.javapro21.service.AccountService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -14,7 +15,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-//@Slf4j
+@Slf4j
 @RestController
 @Tag(name = "Контроллер для работы с аккаунтом")
 @RequestMapping("/api/vi/account")
@@ -29,6 +30,7 @@ public class AccountController {
     @Operation(summary = "регистрация")
     @PostMapping("/register")
     public ResponseEntity<AccountResponse> registration(@RequestBody RegisterRequest registerRequest) throws UserExistException {
+        log.info("Can`t create user with email {} and name {}", registerRequest.getEmail(), registerRequest.getFirstName());
         return new ResponseEntity<>(accountService.registration(registerRequest), HttpStatus.OK);
     }
 }
