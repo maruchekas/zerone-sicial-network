6859fadd3c5ca4cbed419cf92d40908ee4d255f0
Ovechnikov <warlordkolomna@bk.ru>
2022-01-15 23:48:27 +0300

    Changed SQL query, added getDataResponse and getListDataResponse to UtilsService

--- src/main/java/com/skillbox/javapro21/repository/PersonRepository.java
+++ src/main/java/com/skillbox/javapro21/repository/PersonRepository.java
@@ -31,11 +31,19 @@ public interface PersonRepository extends JpaRepository<Person, Integer> {
 
     @Query(value =
             "SELECT * FROM persons " +
-            "WHERE first_name LIKE CONCAT('%', :firstName, '%') " +
+            "WHERE id != :currUserId " +
+                    "AND id NOT IN (" +
+                        "SELECT dst_person_id FROM friendship f " +
+                        "JOIN friendship_statuses fs ON f.status_id = fs.id " +
+                        "WHERE f.src_person_id = :currUserId " +
+                            "AND fs.name IN ('BLOCKED', 'WASBLOCKED', 'INTERLOCKED')" +
+                    ")" +
+                    "AND first_name LIKE CONCAT('%', :firstName, '%') " +
                     "AND last_name LIKE CONCAT('%', :lastName, '%') " +
                     "AND DATE_PART('year', AGE(birth_date)) BETWEEN :ageFrom AND :ageTo " +
                     "AND country LIKE CONCAT('%', :country, '%') " +
-                    "AND town LIKE CONCAT('%', :city, '%')",
+                    "AND town LIKE CONCAT('%', :city, '%')" +
+                    "AND is_blocked = 0",
             nativeQuery = true)
-    Page<Person> findAllByNameAndAgeAndLocation(String firstName, String lastName, Integer ageFrom, Integer ageTo, String country, String city, Pageable page);
+    Page<Person> findAllByNameAndAgeAndLocation(Long currUserId, String firstName, String lastName, Integer ageFrom, Integer ageTo, String country, String city, Pageable page);
 }
