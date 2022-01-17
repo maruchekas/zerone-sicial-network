a2c2c07e15bc9e4016da3dfd241eaa8edd31dcee
maruchekas <salken84@gmail.com>
2022-01-17 15:19:22 +0300

    some chanches in yml and constants

--- src/main/java/com/skillbox/javapro21/config/Constants.java
+++ src/main/java/com/skillbox/javapro21/config/Constants.java
@@ -5,6 +5,11 @@ public final class Constants {
     public static final String CLOUDINARY_AVATARS_FOLDER = "Zerone/Users/";
     public static final String BASE_ROBOTIC_AVA_URL = "https://robohash.org/";
     public static final String AVATAR_CONFIG = ".png?size=360x360&set=set";
+    public static final int CAPTCHA_WIDTH = 193;
+    public static final int CAPTCHA_HEIGHT = 57;
+    public static final int CAPTCHA_LIFESPAN_IN_SEC = 3600;
+    public static final String CAPTCHA_IMG_ENCODE_PREFIX = "data:captchaImage/png;base64, ";
+
 
 
     public enum FileType {
