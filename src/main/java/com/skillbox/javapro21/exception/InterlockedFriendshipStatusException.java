0a76a90977e1119c14a59e9e6318b40a80702715
Ovechnikov <warlordkolomna@bk.ru>
2022-03-05 17:42:59 +0300

    pre-merge

--- src/main/java/com/skillbox/javapro21/exception/InterlockedFriendshipStatusException.java
+++ src/main/java/com/skillbox/javapro21/exception/InterlockedFriendshipStatusException.java
@@ -1,6 +1,6 @@
 package com.skillbox.javapro21.exception;
 
-import static com.skillbox.javapro21.config.Constants.USER_WASBLOCKED_ERR;
+import static com.skillbox.javapro21.config.Constants.USER_INTERLOCKED_ERR;
 
 public class InterlockedFriendshipStatusException extends CustomException {
     public InterlockedFriendshipStatusException() {
