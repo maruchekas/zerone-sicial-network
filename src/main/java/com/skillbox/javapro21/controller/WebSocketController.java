ef293961066f50fe56baa26a0da9447d67936063
Kabirov Radik <kabradman@gmail.com>
2022-03-07 09:06:31 +0000

    Update src/main/java/com/skillbox/javapro21/controller/WebSocketController.java

--- src/main/java/com/skillbox/javapro21/controller/WebSocketController.java
+++ src/main/java/com/skillbox/javapro21/controller/WebSocketController.java
@@ -29,23 +29,4 @@ public class WebSocketController {
     public WSNotificationResponse getNotification(WSNotificationResponse wsNotificationResponse) {
         return wsNotificationResponse;
     }
-
-//    private final Logger logger = LoggerFactory.getLogger(WebSocketController.class);
-//    private final SimpMessagingTemplate messagingTemplate;
-//    private final PersonRepository personRepository;
-//    private final UtilsService utilsService;
-//
-//    int count = 0;
-//    @PreAuthorize("hasAuthority('user:write')")
-//    @PostMapping("/api/v1/messages/{id}")
-//    public String sendMessage(@PathVariable final String id, @RequestBody final MessageTextRequest message, Principal principal) {
-//        Person srcPerson = utilsService.findPersonByEmail(principal.getName());
-//        Person dstPerson = personRepository.findPersonById(Long.parseLong(id)).get();
-//        messagingTemplate.convertAndSendToUser(dstPerson.getEmail(), "/topic/messages", message);
-//        count = count + 1;
-//        messagingTemplate.convertAndSendToUser(dstPerson.getEmail(), "/topic/unreaded", count);
-//
-//        logger.info(srcPerson.getEmail() + " sent message to " + id);
-//        return "ok";
-//    }
 }
