c0fc9885c28e25c589a4636ac1c519ad5954fccb
Kabirov Radik <r4d1@yandex.ru>
2022-03-08 14:42:39 +0500

    Fixed main controller

--- src/main/java/com/skillbox/javapro21/controller/MainController.java
+++ src/main/java/com/skillbox/javapro21/controller/MainController.java
@@ -2,6 +2,7 @@ package com.skillbox.javapro21.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class MainController {
@@ -11,9 +12,14 @@ public class MainController {
         return "index";
     }
 
-//    @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET},
-//            value = "/**/{path:[^\\.]*}")
-//    public String redirectToForward() {
-//        return "forward:/";
+    @RequestMapping(method = {RequestMethod.OPTIONS, RequestMethod.GET},
+            value = "/**/{path:[^\\.]*}")
+    public String redirectToForward() {
+        return "forward:/";
+    }
+
+//    @RequestMapping(value = "/api/v1/socket/")
+//    public String redirectToSocket() {
+//        return "index";
 //    }
 }
